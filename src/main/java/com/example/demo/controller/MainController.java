a6d2ebb01e954e4486d329578618ee1a18fdf7c9
joeblackz <zhangqi13c@163.com>
2022-11-26 12:46:43 +0800

    Change springboot to 3.0.0; And remove prometheus dependency

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,14 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import jakarta.annotation.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.List;
-
 
 @RequestMapping("/")
 @RestController
