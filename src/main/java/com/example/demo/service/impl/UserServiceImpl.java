9a43eca5ccfbc6e2629b70d69b165d56a0f84e5d
joeblackz <zhangqi13c@163.com>
2022-12-14 19:01:39 +0800

    Add Async task

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
